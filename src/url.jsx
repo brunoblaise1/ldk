24d8e51838db0b76b29b6a791f2c459f90d5d937
Blaise <88510730+brunoblaise1@users.noreply.github.com>
2024-12-28 22:22:38 +0200

    update url backend

--- src/url.jsx
+++ src/url.jsx
@@ -1,3 +1,6 @@
 /** @format */
 
-export const url = 'https://ldk-backend.onrender.com';
+//export const url = 'https://ldk-backend.onrender.com';
+
+
+export const url = 'https://ldk-backend-5v71.onrender.com'
